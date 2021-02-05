ef6264c348f37eb1280155e931a9c1c7258e358d
DocSystem <42613513+DocSystem@users.noreply.github.com>
2021-02-05 15:09:26 +0100

    Fin de l'exercice 2

--- 02 - Février/05-02-2021/ex2.py
+++ 02 - Février/05-02-2021/ex2.py
@@ -9,14 +9,24 @@ df2 = df2.sort_values(by="date")
 df2 = df2.sort_values(by="ex")
 
 # Question 3
+print("Oeuvres publiées à partir de 1880 :")
 print(df2[df2.date > 1880])
 
 print(" ")
 
 # Question 4
+print("Oeuvres vendues à plus de 15 exemplaires")
 print(df2[df2.ex > 15])
 
 print(" ")
 
 # Question 5
 print("Nombre de romans : " + str(len(df2)))
+
+print(" ")
+
+# Question 6
+nbr_ex = 0
+for ex in df2.ex:
+    nbr_ex += ex
+print("Nombre total d'exemplaires : " + str(nbr_ex))
