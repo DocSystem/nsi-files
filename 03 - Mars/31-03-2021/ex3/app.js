ae59592059f944b62b1bc097053418574ba2dacb
DocSystem <antoine@soubenfink.com>
2021-03-31 15:56:02 +0200

    MàJ

--- 03 - Mars/31-03-2021/ex3/app.js
+++ 03 - Mars/31-03-2021/ex3/app.js
@@ -3,9 +3,22 @@ function remplirTableau() {
   document.querySelectorAll("td.case").forEach((i) => {
     num = Math.trunc(Math.random()*2);
     i.innerText = num;
+    i.className = "nbr" + num;
+    i.setAttribute("onclick", "changerNbr(this)");
     numList.push(num);
   });
-  console.log(numList);
+  //console.log(numList);
+}
+
+function changerNbr(elem) {
+  if (elem.className == "nbr1") {
+    elem.className = "nbr0";
+    elem.innerText = "0";
+  }
+  else {
+    elem.className = "nbr1";
+    elem.innerText = "1";
+  }
 }
 
 remplirTableau();
